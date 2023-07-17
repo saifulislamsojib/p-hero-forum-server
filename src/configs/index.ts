8bf222c6388e5535a77c9e6e8cc5dabb94e08b21
saifulislamsojib <saifulislam.sojib56@gmail.com>
2023-07-18 02:51:14 +0600

    db fixed

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -6,7 +6,7 @@ env.config();
 const configs = {
   port: process.env.PORT || 5000,
   origin: '*',
-  uri: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@next-hero.ktnme97.mongodb.net/?retryWrites=true&w=majority`,
+  uri: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@next-hero.ktnme97.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
   jwt_secret: process.env.JWT_SECRET || '',
   jwt_expires: 182 * 24 * 60 * 60,
 } as const;
